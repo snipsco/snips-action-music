698f4166a7f79605d6e880d52b724383be9b4146
CoorFun <coorfang@gmail.com>
2019-03-25 18:11:27 +0100

    threshold for intent, slots, asr

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,15 @@ export const LANGUAGE_MAPPINGS = {
     english: 'en',
     french: 'fr'
 }
+export const DEFAULT_MQTT_CONNECTION = {
+    host: 'localhost',
+    port: 1883
+}
+export const DEFAULT_MPD_CONNECTION = {
+    host: 'localhost',
+    port: 6600
+}
+export const SNIPS_PREFIX = 'dfercastrosnipsadmin:'
+export const INTENT_CONFIDENCE_THRESHOLD = 0.7
+export const SLOT_CONFIDENCE_THRESHOLD = 0.5
+export const ASR_TOKENS_CONFIDENCE_THRESHOLD = 0.2
