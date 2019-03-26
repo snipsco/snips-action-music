69b526c61cf07b8af682a9442a435fceaa66c994
CoorFun <coorfang@gmail.com>
2019-03-26 19:24:45 +0100

    play by artist and stop done

--- src/handlers/playMusic.ts
+++ src/handlers/playMusic.ts
@@ -1,12 +1,40 @@
 import { i18nFactory } from '../factories'
 import { Handler } from './index'
 import { logger } from '../utils/logger'
-import { musicInfoExtractor, musicInfoRes } from './musicInfoExtractor'
+import { 
+    musicInfoExtractor,
+    musicInfoRes,
+    getScenario
+} from './playMusicUtils'
 
-export const playMusicHandler: Handler = async function (msg, flow, hermes) {
+export const playMusicHandler: Handler = async function (msg, flow, hermes, player) {
     logger.debug('playMusicHandler')
     
     let music: musicInfoRes = musicInfoExtractor(msg)
+
+    if (!music) {
+        flow.end()
+        return
+    }
+
+    let scenario: string = getScenario(music)
+
+    // Play by song's name
+    if (scenario === 'A') {
+        player.playBySongName(music.songName)
+    }
+
+    if (scenario === 'B') {
+        
+    }
+
+    if (scenario === 'C') {
+        player.byArtistName(music.artistName)
+    }
+
+    if (scenario === 'D') {
+        
+    }
 
     // End the dialog session.
     flow.end()
--- src/handlers/speakerInterrupt.ts
+++ src/handlers/speakerInterrupt.ts
@@ -2,10 +2,10 @@ import { i18nFactory } from '../factories'
 import { Handler } from './index'
 import { logger } from '../utils/logger'
 
-export const speakerInterruptHandler: Handler = async function (msg, flow, hermes) {
+export const speakerInterruptHandler: Handler = async function (msg, flow, hermes, player) {
     logger.debug('speakerInterruptHandler')
     // Ready to be set 
-
+    player.stop()
     flow.end()
 
     // Return the TTS speech.
