3f7b3a926a4f3ef03688ee113532a014f0e4fe7c
CoorFun <coorfang@gmail.com>
2019-03-26 19:07:46 +0100

    scenario pattern mapping

--- src/constants.ts
+++ src/constants.ts
@@ -17,3 +17,22 @@ export const SNIPS_PREFIX = 'dfercastrosnipsadmin:'
 export const INTENT_CONFIDENCE_THRESHOLD = 0.7
 export const SLOT_CONFIDENCE_THRESHOLD = 0.5
 export const ASR_TOKENS_CONFIDENCE_THRESHOLD = 0.2
+// Binary table used to decide scenario by 2 input factor
+// Weight: song - 8, album - 4, artist - 2, playlist - 1
+export const SCENARIO_TABLE = {
+    0b0001: 'D',
+    0b0010: 'C',
+    0b0011: 'C',
+    0b0100: 'B',
+    0b0101: 'B',
+    0b0110: 'B',
+    0b0111: 'B',
+    0b1000: 'A',
+    0b1001: 'A',
+    0b1010: 'A',
+    0b1011: 'A',
+    0b1100: 'A',
+    0b1101: 'A',
+    0b1110: 'A',
+    0b1111: 'A'
+}
