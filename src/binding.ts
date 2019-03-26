52a61c54fe178d95951501831496e95dff5d4ef6
CoorFun <coorfang@gmail.com>
2019-03-26 19:22:26 +0100

    separated callback handler binding

---
+++ src/binding.ts
@@ -0,0 +1,62 @@
+import { Dialog, Hermes } from 'hermes-javascript'
+import { SNIPS_PREFIX } from './constants'
+import handlers from './handlers'
+import { SnipsPlayer } from './snipsPlayer';
+
+export const onIntentDetected = function (hermes: Hermes, player: SnipsPlayer) {
+    const dialog: Dialog = hermes.dialog()
+    dialog.flows([
+        // Music playing request intent
+        {
+            intent: `${SNIPS_PREFIX}playMusic`, 
+            action: (msg, flow) => handlers.playMusic(msg, flow, hermes, player)
+        },
+        // Music controlling intent
+        {
+            intent: `${SNIPS_PREFIX}previousSong`,
+            action: (msg, flow) => handlers.previousSong(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}nextSong`,
+            action: (msg, flow) => handlers.nextSong(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}speakerInterrupt`,
+            action: (msg, flow) => handlers.speakerInterrupt(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}resumeMusic`,
+            action: (msg, flow) => handlers.resumeMusic(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}volumUp`,
+            action: (msg, flow) => handlers.volumUp(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}volumDown`,
+            action: (msg, flow) => handlers.volumDown(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}volumSet`,
+            action: (msg, flow) => handlers.volumSet(msg, flow, hermes, player)
+        },
+        {
+            intent: `${SNIPS_PREFIX}getInfo`,
+            action: (msg, flow) => handlers.getInfo(msg, flow, hermes, player)
+        }
+    ])
+}
+
+export const onSessionToggle = function (hermes: Hermes, player: SnipsPlayer) {
+    const dialog: Dialog = hermes.dialog()
+
+    // Subscribe to system event
+    dialog.on('session_started', message => {
+
+    })
+
+    // Subscribe to system event
+    dialog.on('session_ended', message => {
+        // turn back the music volum
+    })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { withHermes } from 'hermes-javascript'
 import bootstrap from './bootstrap'
-import handlers from './handlers'
 import { translation, logger } from './utils'
 import { SNIPS_PREFIX } from './constants'
+import { 
+    onIntentDetected,
+    onSessionToggle
+} from './binding'
+import { SnipsPlayer } from './SnipsPlayer';
 
 // Initialize hermes
 export default function ({
@@ -10,7 +14,10 @@ export default function ({
         // debug mock
         address: '192.168.171.148:1883'
     },
-    bootstrapOptions = {}
+    bootstrapOptions = {},
+    snipsPlayerOptions = {
+        host: '192.168.171.148'
+    }
 } = {}) : Promise<() => void>{
     return new Promise((resolve, reject) => {
         withHermes(async (hermes, done) => {
@@ -18,51 +25,15 @@ export default function ({
                 // Bootstrap config, locale, i18n…
                 await bootstrap(bootstrapOptions)
 
-                const dialog = hermes.dialog()
+                let musicPlayer = new SnipsPlayer(hermes.dialog(), snipsPlayerOptions)
 
                 logger.debug(`${SNIPS_PREFIX}playMusic`)
 
-                dialog.flows([
-                    // Music playing request intent
-                    {
-                        intent: `${SNIPS_PREFIX}playMusic`, 
-                        action: (msg, flow) => handlers.playMusic(msg, flow, hermes)
-                    },
-                    // Music controlling intent
-                    {
-                        intent: `${SNIPS_PREFIX}previousSong`,
-                        action: (msg, flow) => handlers.previousSong(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}nextSong`,
-                        action: (msg, flow) => handlers.nextSong(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}speakerInterrupt`,
-                        action: (msg, flow) => handlers.speakerInterrupt(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}resumeMusic`,
-                        action: (msg, flow) => handlers.resumeMusic(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}volumUp`,
-                        action: (msg, flow) => handlers.volumUp(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}volumDown`,
-                        action: (msg, flow) => handlers.volumDown(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}volumSet`,
-                        action: (msg, flow) => handlers.volumSet(msg, flow, hermes)
-                    },
-                    {
-                        intent: `${SNIPS_PREFIX}getInfo`,
-                        action: (msg, flow) => handlers.getInfo(msg, flow, hermes)
-                    }
-                ])
-                
+                // subscribe to intent handlers
+                onIntentDetected(hermes, musicPlayer)
+                // subscribe to sessionStarted and sessionEnded
+                onSessionToggle(hermes, musicPlayer)
+    
                 resolve(done)
             } catch (error) {
                 // Output initialization errors to stderr and exit
