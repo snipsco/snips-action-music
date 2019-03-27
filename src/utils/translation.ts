29ac60d8712a793d31c4f1026338f8f08867e64c
CoorFun <coorfang@gmail.com>
2019-03-27 18:29:33 +0100

    always various tts playing

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -1,6 +1,18 @@
 import { i18nFactory } from '../factories/i18nFactory'
+import { logger } from '../utils/logger'
+
+function randomTranslation (key: string | string[], opts: {[key: string]: any}): string {
+    const i18n = i18nFactory.get()
+    const possibleValues = i18n(key, { returnObjects: true, ...opts })
+    if(typeof possibleValues === 'string')
+        return possibleValues
+    const randomIndex = Math.floor(Math.random() * possibleValues.length)
+    return possibleValues[randomIndex]
+}
 
 export const translation = {
+    // Takes an array from the i18n and returns a random item.
+    randomTranslation,
     // Outputs an error message based on the error object, or a default message if not found.
     errorMessage: async (error: Error): Promise<string> => {
         let i18n = i18nFactory.get()
@@ -11,18 +23,9 @@ export const translation = {
         }
 
         if(i18n) {
-            return i18n([`error.${error.message}`, 'error.unspecific'])
+            return randomTranslation(`error.${error.message}`, {})
         } else {
             return 'Oops, something went wrong.'
         }
-    },
-    // Takes an array from the i18n and returns a random item.
-    randomTranslation (key: string | string[], opts: {[key: string]: any}): string {
-        const i18n = i18nFactory.get()
-        const possibleValues = i18n(key, { returnObjects: true, ...opts })
-        if(typeof possibleValues === 'string')
-            return possibleValues
-        const randomIndex = Math.floor(Math.random() * possibleValues.length)
-        return possibleValues[randomIndex]
     }
 }
