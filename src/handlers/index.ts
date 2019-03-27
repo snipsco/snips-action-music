c226bc472e8ccc5ed96a1aadb3fa3936819eed48
CoorFun <coorfang@gmail.com>
2019-03-27 18:24:19 +0100

    check confidence error before drop to handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,4 @@
-import { translation, logger } from '../utils'
+import { translation, logger, message } from '../utils'
 import { FlowContinuation, IntentMessage, FlowActionReturn } from 'hermes-javascript'
 
 import { playMusicHandler } from './playMusic'
@@ -11,6 +11,12 @@ import { volumDownHandler } from './volumDown'
 import { volumSetHandler } from './volumSet'
 import { getInfoHandler } from './getInfo'
 
+import {
+    INTENT_CONFIDENCE_STANDERD,
+    INTENT_CONFIDENCE_BAD,
+    ASR_TOKENS_CONFIDENCE_THRESHOLD
+} from '../constants'
+
 export type Handler = (
     message: IntentMessage,
     flow: FlowContinuation,
@@ -19,17 +25,27 @@ export type Handler = (
 
 // Wrap handlers to gracefully capture errors
 const handlerWrapper = (handler: Handler): Handler => (
-    async (message, flow, ...args) => {
-        //logger.debug('message: %O', message)
+    async (msg, flow, ...args) => {
+        //logger.debug('message: %O', msg)
         try {
-            // Run handler until completion
-            const tts = await handler(message, flow, ...args)
-            // And make the TTS speak
+            // Check confidenceScore before call the handler
+            if (msg.intent.confidenceScore < INTENT_CONFIDENCE_BAD) {
+                throw new Error('nluIntentErrorBad')
+            }
+
+            if (msg.intent.confidenceScore < INTENT_CONFIDENCE_STANDERD) {
+                throw new Error('nluIntentErrorStanderd')
+            }
+        
+            if (message.getAsrConfidence(msg) < ASR_TOKENS_CONFIDENCE_THRESHOLD) {
+                throw new Error('asrError')
+            }
+
+            const tts = await handler(msg, flow, ...args)
+
             return tts
         } catch (error) {
-            // If an error occurs, end the flow gracefully
             flow.end()
-            // And make the TTS output the proper error message
             logger.error(error)
             return await translation.errorMessage(error)
         }
