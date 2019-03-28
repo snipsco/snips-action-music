c1d07be0e7ba373cd859f0b17db7251db750e342
CoorFun <coorfang@gmail.com>
2019-03-28 17:24:16 +0100

    make async

--- src/handlers/sessionEnded.ts
+++ src/handlers/sessionEnded.ts
@@ -1,9 +1,9 @@
 import { Hermes, SessionEndedMessage } from 'hermes-javascript'
 import { SnipsPlayer } from '../snipsPlayer';
 
-export const sessionEnded = function(msg: SessionEndedMessage, hermes: Hermes, player: SnipsPlayer){
+export const sessionEnded = async function(msg: SessionEndedMessage, hermes: Hermes, player: SnipsPlayer){
     // turn back the music volume 
-    player.setVolumeToNormal()
+    await player.setVolumeToNormal()
 
     // turn back the feedback sound
 }
--- src/handlers/sessionStarted.ts
+++ src/handlers/sessionStarted.ts
@@ -1,9 +1,9 @@
 import { Hermes, SessionStartedMessage } from 'hermes-javascript'
 import { SnipsPlayer } from '../snipsPlayer';
 
-export const sessionStarted = function(msg: SessionStartedMessage, hermes: Hermes, player: SnipsPlayer){
+export const sessionStarted = async function(msg: SessionStartedMessage, hermes: Hermes, player: SnipsPlayer){
     // lower the music volum 
-    player.setVolumeToSilence()
+    await player.setVolumeToSilence()
 
     // turn off the feedback sound
 }
