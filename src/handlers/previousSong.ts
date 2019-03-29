42e43693e1d5c7da5a6ed86e14a6fc668006a734
CoorFun <coorfang@gmail.com>
2019-03-29 13:54:14 +0100

    change

--- src/handlers/previousSong.ts
+++ src/handlers/previousSong.ts
@@ -5,7 +5,7 @@ import { logger } from '../utils/logger'
 export const previousSongHandler: Handler = async function (msg, flow, hermes, player) {
     logger.debug('previousSongHandler')
     flow.end()
-    // To confirm: stop/ pause
+    
     player.previous().then(
         logger.info('Playing the previous song')
     )
