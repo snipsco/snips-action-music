acfddc7c985a96084435e0658fa3a8209df34c0a
CoorFun <coorfang@gmail.com>
2019-04-15 17:21:38 +0200

    give more pricise query info

--- src/handlers/getInfo.ts
+++ src/handlers/getInfo.ts
@@ -1,10 +1,20 @@
 import { Handler } from './index'
-import { logger, translation } from '../utils'
+import { logger, translation, message } from '../utils'
+import { NluSlot, slotType } from 'hermes-javascript'
+import { 
+    SLOT_CONFIDENCE_THRESHOLD 
+} from '../constants'
 
 export const getInfoHandler: Handler = async function (msg, flow, hermes, player) {
     logger.debug('getInfoHandler')
     flow.end()
 
+    const slot: NluSlot<slotType.custom> | null = message.getSlotsByName(msg, 'music_item', {
+        onlyMostConfident: true,
+        threshold: SLOT_CONFIDENCE_THRESHOLD
+    })
+    const slotValue = slot ? slot.value.value : null
+    
     const info = await player.getPlayingInfo()
 
     if (!info) {
@@ -12,9 +22,28 @@ export const getInfoHandler: Handler = async function (msg, flow, hermes, player
     }
 
     logger.debug(info)
-
-    return translation.randomTranslation('info.playTrackArtist', {
-        track: info.title,
-        artist: info.artist
-    })
+    logger.debug('asking: ', slotValue)
+    switch (slotValue) {
+        case 'track':
+            return translation.randomTranslation('info.playTrack', {
+                track: info.title
+            })
+        case 'song':
+            return translation.randomTranslation('info.playTrack', {
+                track: info.title
+            })
+        case 'artist':
+            return translation.randomTranslation('info.playArtist', {
+                artist: info.artist
+            })
+        case 'album':
+            return translation.randomTranslation('info.playAlbum', {
+                album: info.album
+            })
+        default:
+            return translation.randomTranslation('info.playTrackArtist', {
+                track: info.title,
+                artist: info.artist
+            })
+    }
 }
