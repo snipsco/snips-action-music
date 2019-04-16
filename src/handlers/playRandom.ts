0429174156020d1fa038933f7255b73200368585
CoorFun <coorfang@gmail.com>
2019-04-16 12:01:27 +0200

    no tts for play random

--- src/handlers/playRandom.ts
+++ src/handlers/playRandom.ts
@@ -8,17 +8,10 @@ export const playRandomHandler: Handler = async function (msg, flow, hermes, pla
 
     const playlistRaw: string = await player.getLoadedPlaylistRandom()
     const playlist: string = playlistRaw.replace('.m3u', '')
-    
+
     logger.debug('found playlist: ', playlist)
 
     await player.loadPlaylistIfPossible(playlist)
 
     await player.play()
-
-    const info = await player.getPlayingInfo()
-
-    return translation.randomTranslation('info.playTrackArtist', {
-        track: info.title,
-        artist: info.artist
-    })
 }
