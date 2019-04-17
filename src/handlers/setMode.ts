675a631cb9c4b038d35dff3686f60a0ba404448c
CoorFun <coorfang@gmail.com>
2019-04-17 17:00:06 +0200

    calling the new api to change the mode

--- src/handlers/setMode.ts
+++ src/handlers/setMode.ts
@@ -17,21 +17,5 @@ export const setModeHandler: Handler = async function (msg, flow, hermes, player
         throw new Error('noSlotValueFound')
     }
 
-    switch (mode) {
-        case 'random':
-            await player.setToRandom()
-            return 
-        case 'single':
-            await player.setToSingle()
-            return
-        case 'repeat':
-            await player.setToRepeat()
-            return 
-        case 'sequence':
-            await player.setToSequence()
-            return
-        case 'default':
-            await player.setToSequence()
-            return
-    }
+    await player.setMode(mode)
 }
